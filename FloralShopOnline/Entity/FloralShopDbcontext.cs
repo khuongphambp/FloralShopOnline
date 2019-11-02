4acf610c3461f9d1606a27e42cd0623fee1a2a1c
khuongphambp <khuongphambp@gmail.com>
2019-11-02 20:14:22 +0700

    jjuuj

---
+++ FloralShopOnline/Entity/FloralShopDbcontext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FloralShopOnline.Entity
+{
+    public class FloralShopDbcontext : DbContext
+    {
+        public DbSet<TypeProduct> TypeProducts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=FlowerShopDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ FloralShopOnline/Migrations/20191102131313_khuong.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FloralShopOnline.Migrations
+{
+    public partial class khuong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TypeProducts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeProducts", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TypeProducts");
+        }
+    }
+}
